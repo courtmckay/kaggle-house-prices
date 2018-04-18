16f23a36d1babaa445a7b2356bd47f3207d12cdc
layliana15 <13956255+layliana15@users.noreply.github.com>
2018-04-18 14:19:48 -0400

    Starting Kaggle competition project to predict housing prices

---
+++ 0-Load.R
@@ -0,0 +1,17 @@
+# 0-Load.R
+  # Part of project to go through first Kaggle competition which aims at predicting house prices. Results and explorations will
+  #be compiled in .Rmd notebook titled 'Housing Prices.Rmd'
+
+
+#-------- Set up environment
+
+
+
+
+
+#------- Load data
+#I've downloaded the test and train data sets and have saved them directly in the R project file associated with this project
+
+
+
+
